19461cba82d4fe853c7d24ab18d5565147570ad2
mobile4mx <30882713+mobile4mx@users.noreply.github.com>
2017-08-09 21:31:56 -0500

    Start Project

---
+++ Cliente/app/src/main/java/mx/mobile4/cliente/MainActivity.java
@@ -0,0 +1,15 @@
+package mx.mobile4.cliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this.getApplicationContext(), "Mobile4 Innovando", Toast.LENGTH_SHORT).show();
+    }
+}
